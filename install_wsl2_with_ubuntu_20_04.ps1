972850badd5d4f6ab2967f515752b4c80d49f16e
Barroso Filho <barrosofilho.ti@gmail.com>
2023-03-16 23:15:46 -0300

    chore: rename install_wsl_with_ubuntu.ps1 to install_wsl2_with_ubuntu_20_04.ps1

--- install_wsl2_with_ubuntu_20_04.ps1
+++ install_wsl2_with_ubuntu_20_04.ps1
@@ -1,4 +1,4 @@
-# Este script instala e configura o WSL2 com Ubuntu 20.10 no PowerShell
+# Este script instala e configura o WSL2 com Ubuntu 20.04 no PowerShell
 
 # Função para exibir mensagens formatadas
 function Write-Info {
@@ -25,12 +25,12 @@ Write-Info "Verificando o status do WSL..."
 if (Get-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" | Where-Object -Property State -eq "Enabled") {
     Write-Info "WSL já está instalado e funcionando."
     
-    # Verificando se o Ubuntu 20.10 está instalado
+    # Verificando se o Ubuntu 20.04 está instalado
     $ubuntuInstalled = $false
     try {
         $distros = (wsl.exe -l -q)
         foreach ($distro in $distros) {
-            if ($distro -match "Ubuntu-20.10") {
+            if ($distro -match "Ubuntu-20.04") {
                 $ubuntuInstalled = $true
                 break
             }
@@ -38,16 +38,16 @@ if (Get-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem
     } catch { }
 
     if ($ubuntuInstalled) {
-        Write-Info "Ubuntu 20.10 já está instalado no WSL."
+        Write-Info "Ubuntu 20.04 já está instalado no WSL2."
 
         # Informando como abrir o WSL
-        Write-Host "Para abrir o WSL com Ubuntu 20.10, digite 'wsl' ou 'ubuntu2010' no PowerShell ou no prompt de comando." -ForegroundColor Green
+        Write-Host "Para abrir o WSL com Ubuntu 20.04, digite 'wsl' ou 'ubuntu2004' no PowerShell ou no prompt de comando." -ForegroundColor Green
         WaitForEscOrEnter
     }
 } 
 
-# Se o WSL não está instalado ou o Ubuntu 20.10 não foi encontrado, prossiga com a instalação
-Write-Info "WSL não encontrado ou Ubuntu 20.10 não instalado. Iniciando a instalação do WSL2 e Ubuntu 20.10..."
+# Se o WSL não está instalado ou o Ubuntu 20.04 não foi encontrado, prossiga com a instalação
+Write-Info "WSL não encontrado ou Ubuntu 20.04 não instalado. Iniciando a instalação do WSL2 e Ubuntu 20.04..."
 Write-Info "Isso pode levar algum tempo, por favor aguarde."
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
@@ -63,16 +63,21 @@ Start-Process -FilePath msiexec -ArgumentList "/i", $DownloadPath, "/quiet", "/q
 Write-Info "Configurando o WSL2 como padrão..."
 wsl --set-default-version 2
 
-# Instalando o Ubuntu 20.10
-Write-Info "Instalando o Ubuntu 20.10..."
-$UbuntuUrl = "https://aka.ms/wsl-ubuntu-2010"
-$DownloadPath = "$env:TEMP\Ubuntu_2010.appx"
+# Instalando o Ubuntu 20.04
+Write-Info "Instalando o Ubuntu 20.04..."
+$UbuntuUrl = "https://aka.ms/wslubuntu2004"
+$DownloadPath = "$env:TEMP\Ubuntu_2004.appx"
 Invoke-WebRequest -Uri $UbuntuUrl -OutFile $DownloadPath
-Add-AppxPackage -Path $DownloadPath
+try {
+    Add-AppxPackage -Path $DownloadPath
+} catch {
+    Write-Host "Não foi possível instalar o Ubuntu 20.04 usando Add-AppxPackage. Tentando outra abordagem..." -ForegroundColor Yellow
+    New-WSLDistro -DistributionName Ubuntu-20.04 -Download -Verbose
+}
 
 # Mensagem de sucesso
-Write-Host "WSL2 e Ubuntu 20.10 instalados com sucesso!" -ForegroundColor Green
+Write-Host "WSL2 e Ubuntu 20.04 instalados com sucesso!" -ForegroundColor Green
 
 # Informando como executar o WSL2
-Write-Host "Para executar o WSL2 e o Ubuntu 20.10, digite 'wsl' ou 'ubuntu2010' no PowerShell ou no prompt de comando." -ForegroundColor Green
+Write-Host "Para executar o WSL2 e o Ubuntu 20.04, digite 'wsl' ou 'ubuntu2004' no PowerShell ou no prompt de comando." -ForegroundColor Green
 WaitForEscOrEnter
