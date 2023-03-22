2f276fac4f97ca733aa47891104ff920c3b932b8
Barroso Filho <barrosofilho.ti@gmail.com>
2023-03-22 00:52:31 -0300

    update install_wsl2_ubuntu_20_04_docker.ps1

--- install_wsl2_ubuntu_20_04_docker.ps1
+++ install_wsl2_ubuntu_20_04_docker.ps1
@@ -1,4 +1,5 @@
 $OutputEncoding = [System.Text.Encoding]::UTF8
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
 function WaitForEscOrEnter {
     Write-Host ""
@@ -10,18 +11,10 @@ function WaitForEscOrEnter {
     exit
 }
 
-# Verifica se o PowerShell está sendo executado como administrador
 function IsAdmin {
     $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = New-Object Security.Principal.WindowsPrincipal($identity)
     $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-}
-
-# Verifica se o usuário está executando o script como administrador
-if (-not (IsAdmin)) {
-    Write-Host "Este script precisa ser executado como administrador." -ForegroundColor Red
-    Write-Host "Por favor, clique com o botão direito do mouse deste arquivo, procure e selecione a opção 'Executar como administrador' ou 'Run as Administrator'." -ForegroundColor Yellow
-    WaitForEscOrEnter
 }
 
 function Write-Info {
@@ -30,16 +23,17 @@ function Write-Info {
     Write-Host $Message -ForegroundColor Cyan
 }
 
-# Verificando se o Ubuntu 20.04 esta instalado
-$ubuntuInstalled = $true
+function InstallDocker {
+    Write-Host "Ok! Já que o WSL e o Ubuntu 20.04 estão instalados corretamente, vamos agora instalar o Docker CE!"
+    wsl.exe -d Ubuntu-20.04 --exec sh -c "wget -O ~/docker_install.sh https://raw.githubusercontent.com/barrosohub/docker_ce_ubuntu_20_04/main/install.sh"
+    wsl.exe -d Ubuntu-20.04 --exec sh -c "chmod +x ~/docker_install.sh && ~/docker_install.sh && rm ~/docker_install.sh && sudo service docker start"
+    Write-Host "============================================================" -ForegroundColor Green
+    Write-Host " Docker CE (Community Edition) instalado com sucesso!" -ForegroundColor Green
+    Write-Host "============================================================" -ForegroundColor Green
+    RedirectIfDockerIsInstalled
+}
 
-Write-Info "===================================================================="
-Write-Info "Verificando o status do WSL... Aguarde, pode demorar alguns minutos!"
-Write-Info "===================================================================="
-Write-Host ""
-$wslFeature = Get-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux"
-
-function redirectIfDockerIsInstalled {
+function RedirectIfDockerIsInstalled {
     Write-Host ""
     Write-Host "Aguarde 5 segundos... Estamos redirecionando para o terminal do WSL..." -ForegroundColor Yellow
     Write-Host ""
@@ -47,30 +41,43 @@ function redirectIfDockerIsInstalled {
     wsl.exe -d Ubuntu-20.04
 }
 
+if (-not (IsAdmin)) {
+    Write-Host "Este script precisa ser executado como administrador." -ForegroundColor Red
+    Write-Host "Por favor, clique com o botão direito do mouse deste arquivo, procure e selecione a opção 'Executar como administrador' ou 'Run as Administrator'." -ForegroundColor Yellow
+    WaitForEscOrEnter
+}
+
+$wslFeature = Get-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux"
+
+Write-Info "===================================================================="
+Write-Info "Verificando o status do WSL... Aguarde, pode demorar alguns minutos!"
+Write-Info "===================================================================="
+Write-Host ""
+
 if ($wslFeature.State -eq "Enabled") {
     Write-Host "WSL2 está instalado. " -NoNewLine
     Write-Host "[OK]" -ForegroundColor Green
 
-    if ($ubuntuInstalled) {        
+    $ubuntuInstalled = wsl.exe -l -q | Select-String -Pattern "Ubuntu-20.04"
+    $ubuntuInitialized = wsl.exe -d Ubuntu-20.04 --exec sh -c "test -f ~/.ubuntu_initialized && echo true || echo false"
+
+    if ($ubuntuInstalled -and $ubuntuInitialized -eq "true") {
         Write-Host "Notamos que o Ubuntu 20.04 está instalado corretamente no WSL. " -NoNewLine
         Write-Host "[OK]" -ForegroundColor Green
 
-       # Verificando se o Docker está instalado no WSL
-    $dockerCheck = wsl.exe -d Ubuntu-20.04 --exec sh -c "systemctl is-active docker"
-    if ($dockerCheck -eq "active") {
-        Write-Host "Notamos que o Docker CE (Community Edition) está instalado corretamente no WSL. " -NoNewLine
-        Write-Host "[OK]" -ForegroundColor Green
-        redirectIfDockerIsInstalled
+        # Verificando se o Docker está instalado no WSL
+        $dockerCheck = wsl.exe -d Ubuntu-20.04 --exec sh -c "systemctl is-active docker"
+        if ($dockerCheck -eq "active") {
+            Write-Host "Notamos que o Docker CE (Community Edition) está instalado corretamente no WSL. " -NoNewLine
+            Write-Host "[OK]" -ForegroundColor Green
+            RedirectIfDockerIsInstalled
+        } else {
+            InstallDocker
+        }
+    } elseif ($ubuntuInstalled) {
+        Write-Host "Ubuntu 20.04 está instalado, mas não foi inicializado. Por favor, inicialize o Ubuntu 20.04 e configure o usuário e senha. Após isso, execute este script novamente." -ForegroundColor Yellow
     } else {
-        Write-Host "Ok! Já que o WSL e o Ubuntu 20.04 estão instalados corretamente, vamos agora instalar o Docker CE!"
-        wsl.exe -d Ubuntu-20.04 --exec sh -c "wget -O ~/docker_install.sh https://raw.githubusercontent.com/barrosohub/docker_ce_ubuntu_20_04/main/install.sh"
-        wsl.exe -d Ubuntu-20.04 --exec sh -c "chmod +x ~/docker_install.sh && ~/docker_install.sh && rm ~/docker_install.sh && sudo service docker start"
-        Write-Host "============================================================" -ForegroundColor Green
-        Write-Host " Docker CE (Community Edition) instalado com sucesso!" -ForegroundColor Green
-        Write-Host "============================================================" -ForegroundColor Green
-        redirectIfDockerIsInstalled
-    }
-
+        Write-Host "Ubuntu 20.04 não está instalado. Por favor, instale o Ubuntu 20.04 e inicialize-o. Após isso, execute este script novamente." -ForegroundColor Yellow
     }
 } else {
     Write-Info "Iniciando a configuracao/verificacao do WSL2 e Ubuntu 20.04..."
@@ -83,27 +90,20 @@ if ($wslFeature.State -eq "Enabled") {
     Invoke-WebRequest -Uri $KernelUrl -OutFile $DownloadPath
     Start-Process -FilePath msiexec -ArgumentList "/i", $DownloadPath, "/quiet", "/qn", "/norestart" -Wait
 
-    wsl --set-default-version 2
+    wsl --set-default-version 2 > Out-Null
 
-    try {
-        wsl.exe --install -d Ubuntu-20.04
-        Start-Sleep -Seconds 30
-       
-    wsl.exe --set-version Ubuntu-20.04 2
-} catch {
-    Write-Host "Nao foi possivel instalar o Ubuntu 20.04 usando 'wsl --install -d Ubuntu-20.04'. Tentando outra abordagem..." -ForegroundColor Yellow
     $UbuntuUrl = "https://aka.ms/wslubuntu2004"
     $DownloadPath = "$env:TEMP\Ubuntu_2004.appx"
     Invoke-WebRequest -Uri $UbuntuUrl -OutFile $DownloadPath
     Add-AppxPackage -Path $DownloadPath
-}
-Write-Host ""
-Write-Host "============================================"
-Write-Host " WSL2 e Ubuntu 20.04 instalados com sucesso!" -ForegroundColor Green
-Write-Host "============================================"
-Write-Host ""
-Write-Host " Reinicie o computador para concluir a configuração do WSL2! Após isso, execute esse script novamente para prosseguirmos para a configuração do Ubuntu e instalação do Docker" -ForegroundColor Yellow
-Write-Host ""
+
+    Write-Host ""
+    Write-Host "============================================"
+    Write-Host " WSL2 e Ubuntu 20.04 instalados com sucesso!" -ForegroundColor Green
+    Write-Host "============================================"
+    Write-Host ""
+    Write-Host " Reinicie o computador para concluir a configuração do WSL2! Após isso, execute esse script novamente para prosseguirmos para a configuração do Ubuntu e instalação do Docker" -ForegroundColor Yellow
+    Write-Host ""
 }
 
 WaitForEscOrEnter
